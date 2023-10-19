f992dc0f5ca77b0923c512b6066940d98620a722
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-19 18:56:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
- import './App.css';
-import data from './data.json'
-function  App() {
-   const videoElement = document.getElementById('root')
-  const canvas = document.getElementById('canvas')
-  canvas.width = videoElement.clientWidth
-  canvas.height = videoElement.clientHeight
-  const context = canvas.getContext('2d')
-  
-  videoElement.parentNode.insertBefore(canvas, videoElement)
+import data from "./data.json";
+function App() {
+  const videoElement = document.getElementById("root");
+  const canvas = document.getElementById("canvas");
 
+  function resizeCanvas() {
+    canvas.width = videoElement.clientWidth;
+    canvas.height = videoElement.clientHeight;
+  }
+
+  videoElement.parentNode.insertBefore(canvas, videoElement);
 
   function drawBoxes() {
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const frameRate = data.fps
+    const frameRate = data.fps;
     const currentTimeInSeconds = videoElement.currentTime;
     const currentFrame = Math.floor(currentTimeInSeconds * frameRate);
-    context.clearRect(0,0,canvas.width, canvas.height)
-
 
     function drawBoxesForObject(objectId) {
       const objectData = data.supertracks[objectId];
-      
+
       if (objectData && objectData.tracks_data) {
         const tracksData = objectData.tracks_data;
         for (const trackData of tracksData) {
-          if (currentFrame >= trackData.frame_start && currentFrame <= trackData.frame_end) {
-       console.log('bingo!')
+          if (
+            currentFrame >= trackData.frame_start &&
+            currentFrame <= trackData.frame_end
+          ) {
+            console.log("bingo!");
             const trackId = trackData.track_id;
             const trackInfo = data.tracks[trackId];
             if (trackInfo) {
               const frameData = trackInfo[currentFrame];
               if (frameData) {
                 const [x1, y1, x2, y2] = frameData;
-                
-                const newBoxX1 = x1 + 105;
-                const newBoxX2 = x2 + 105;
-                context.strokeStyle = 'blue';
+
+                context.strokeStyle = "blue";
                 context.lineWidth = 2;
-                context.strokeRect(newBoxX1, y1, newBoxX2 - newBoxX1, y2 - y1);
+                context.strokeRect(x1, y1, x2 - x1, y2 - y1);
               }
             }
           }
@@ -45,45 +46,35 @@ function  App() {
       }
     }
 
-
     const objectIds = Object.keys(data.supertracks);
-    objectIds.forEach(objectId => {
+    objectIds.forEach((objectId) => {
       drawBoxesForObject(objectId);
     });
 
     requestAnimationFrame(drawBoxes);
   }
 
-// Начните отрисовку боксов при воспроизведении видео
-videoElement.addEventListener("play", function () {
-  drawBoxes();
-});
+  videoElement.addEventListener("play", function () {
+    resizeCanvas();
+    drawBoxes();
+  });
 
-// Остановите отрисовку боксов при паузе видео
-videoElement.addEventListener("pause", function () {
-//  cancelAnimationFrame(requestId)
-});
+  window.addEventListener("resize", resizeCanvas);
 
-// Воспользуйтесь этим кодом, чтобы начать и остановить отрисовку при кликах на видео
-videoElement.addEventListener("click", function () {
-  if (videoElement.paused) {
-    videoElement.play();
-  } else {
-    videoElement.pause();
-  }
-});
+  videoElement.addEventListener("pause", function () {
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
 
+  videoElement.addEventListener("click", function () {
+    if (videoElement.paused) {
+      videoElement.play();
+    } else {
+      videoElement.pause();
+    }
+  });
 
-
- 
- 
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
